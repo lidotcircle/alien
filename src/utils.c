fb93e91a8798f6d6f5f52b4b8e52e8e958b54f31
analyman <beam.analyman@gmail.com>
2021-10-16 08:23:02 +0000

    save

--- src/utils.c
+++ src/utils.c
@@ -12,10 +12,33 @@ struct function_list {
 };
 
 #if defined(WINDOWS)
+#include <winnt.h>
+
 function_list* lf_load(const void* lib)
 {
-    fprintf(stderr, "not implemented\n");
-    exit(1);
+    assert(((PIMAGE_DOS_HEADER)lib)->e_magic == IMAGE_DOS_SIGNATURE);
+    PIMAGE_NT_HEADERS header = (PIMAGE_NT_HEADERS)((BYTE *)lib + ((PIMAGE_DOS_HEADER)lib)->e_lfanew);
+    assert(header->Signature == IMAGE_NT_SIGNATURE);
+    assert(header->OptionalHeader.NumberOfRvaAndSizes > 0);
+    PIMAGE_EXPORT_DIRECTORY exports = (PIMAGE_EXPORT_DIRECTORY)((BYTE *)lib + header->
+            OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+    assert(exports->AddressOfNames != 0);
+    BYTE** names = (BYTE**)((int)lib + exports->AddressOfNames);
+
+    function_list* fl = (function_list*)(malloc(sizeof(function_list)));
+    fl->n = exports->NumberOfNames;
+    fl->names = (char**)malloc(fl->n * sizeof(char*));
+    memset(fl->names, 0, fl->n * sizeof(char*));
+
+    for (int i = 0; i < exports->NumberOfNames; i++) {
+        const char* str = (char*)((BYTE*)lib + (int)names[i]);
+        size_t m = strlen(str);
+        fl->names[i] = (char*)malloc(m + 1);
+        memcpy(fl->names[i], str, m);
+        fl->names[i][m] = '\0';
+    }
+
+    return fl;
 }
 #elif defined(linux)
 #define _GNU_SOURCE
