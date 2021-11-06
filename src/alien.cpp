7fbf3bfbf0c0020aba0b9e90b68125ad43bc84cb
analyman <beam.analyman@gmail.com>
2021-11-06 06:37:09 +0000

    it should be that dlopen memory leak detected by valgrind cause by resolving libc++ symbols

--- src/alien.cpp
+++ src/alien.cpp
@@ -1,7 +1,23 @@
 #include <assert.h>
 #include <string>
 // #include <iostream> cause leak in valgrind
+#include <ostream>
+#include <istream>
+#include <sstream>
+#include <codecvt>
+#include <locale>
 #include <lua.hpp>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <set>
+#include <memory>
+#include <stdarg.h>
+#include <stdexcept>
+#include <tuple>
+#include <stdio.h>
+#include <fstream>
 
 class 
 //__attribute__((visibility("hidden"))) compile options -fvisibility=hidden
@@ -9,6 +25,7 @@ CL
 {
     public:
         int cln();
+        int xx() { throw std::runtime_error("xx"); }
 };
 
 int CL::cln() {return 1;}
@@ -22,6 +39,9 @@ __attribute__((visibility("default")))
 int luaopen_alien_c(lua_State *L) {
     /* Register main library */
     luaL_newlib(L, alienlib);
+
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    std::wstring str = converter.from_bytes("hello");
 
     return CL().cln();
 }
